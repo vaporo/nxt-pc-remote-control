91c057856922a9afa026742b1decc3f57a3d60c6
felipero25 <felipero25@c60f1a58-b76a-3775-8db6-2cf6b399e817>
2013-06-12 14:56:59 +0000

    launch new options and new interfaces

--- idiom.h
+++ idiom.h
@@ -29,6 +29,18 @@ public:
   QString messageNearDivices[2];
   QString messageDeviceAvailable[2];
   QString imageInfo[2];
+  QString options[2];
+  QString motorsInUse[2];
+  QString configurations[2];
+  QString active[2];
+  QString add[2];
+  QString remove[2];
+  QString save[2];
+  QString left[2];
+  QString right[2];
+  QString loadFile[2];
+  QString saveFile[2];
+
 public:
 
   /** ----------------------------------------------------------------------
@@ -80,6 +92,40 @@ public:
 
     imageInfo[ENG] = ":/images/info-eng.png";
     imageInfo[SPA] = ":/images/info-spa.png";
+
+    options[ENG] = "Options";
+    options[SPA] = "Opciones";
+
+    motorsInUse[ENG] = "Motors in use";
+    motorsInUse[SPA] = "Motores en funcionamiento";
+
+    configurations[ENG] = "Configurations";
+    configurations[SPA] = "Configuraciones";
+
+    active[ENG] = "Active";
+    active[SPA] = "Activo";
+
+    add[ENG] = "Add";
+    add[SPA] = "Agregar";
+
+    remove[ENG] = "Remove";
+    remove[SPA] = "Remover";
+
+    save[ENG] = "Save";
+    save[SPA] = "Guardar";
+
+    left[ENG] = "Left";
+    left[SPA] = "Izquierda";
+
+    right[ENG] = "Right";
+    right[SPA] = "Derecha";
+
+    loadFile[ENG] = "Load File";
+    loadFile[SPA] = "Abrir Archivo";
+
+    saveFile[ENG] = "Save File";
+    saveFile[SPA] = "Guardar Archivo";
+
   }
 
   /** ----------------------------------------------------------------------
@@ -123,6 +169,18 @@ public:
   QString getMessageNearDivices()       { return messageNearDivices[it]; }
   QString getMessageDeviceAvailable()   { return messageDeviceAvailable[it]; }
   QString getImageInfo()                { return imageInfo[it]; }
+  QString getOptions()                  { return options[it]; }
+  QString getMotorsInUse()              { return motorsInUse[it]; }
+  QString getConfigurations()           { return configurations[it]; }
+  QString getActive()                   { return active[it]; }
+  QString getAdd()                      { return add[it]; }
+  QString getRemove()                   { return remove[it]; }
+  QString getSave()                     { return save[it]; }
+  QString getLeft()                     { return left[it]; }
+  QString getRight()                    { return right[it]; }
+  QString getLoadFile()                 { return loadFile[it]; }
+  QString getSaveFile()                 { return saveFile[it]; }
+
 };
 
 #endif // IDIOM_H
